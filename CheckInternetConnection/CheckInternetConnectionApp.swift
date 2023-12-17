e481d47e5b87975a3dfcc227b945bab9f201a58e
Fatih Durmaz <fatih.durmaz@outlook.com>
2023-12-17 23:48:35 +0300

    Initial Commit

---
+++ CheckInternetConnection/CheckInternetConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInternetConnectionApp.swift
+//  CheckInternetConnection
+//
+//  Created by Fatih Durmaz on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckInternetConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckInternetConnection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckInternetConnection
+//
+//  Created by Fatih Durmaz on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
